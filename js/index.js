70b19696cb216099aac7e1d8d0b1a88f11d2dfdc
shela1216 <shela1216@gmail.com>
2021-08-31 14:24:09 +0800

    lineLogn

--- js/index.js
+++ js/index.js
@@ -208,6 +208,33 @@
                     this.sortListData();
                 }
 
+            },
+            passRadom:function(){
+                var text = [
+                    "abcdefghijklmnopqrstuvwxyz",
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "1234567890",
+                  ];
+                  var rand = function (min, max) {
+                    return Math.floor(Math.max(min, Math.random() * (max + 1)));
+                  };
+                  var len = rand(8, 16); // 长度为8-16
+                  var pw = "";
+                  for (i = 0; i < len; ++i) {
+                    var strpos = rand(0, 2);
+                    pw += text[strpos].charAt(rand(0, text[strpos].length));
+                  }
+                  return pw;
+            },
+            LineLogin:function(){
+                var pass = this.passRadom();
+                var URL = 'https://access.line.me/oauth2/v2.1/authorize?';
+                URL += 'response_type=code';
+                URL += '&client_id=1656361556';
+                URL += '&redirect_uri=/customer/auth/line/callback';
+                URL += '&state='+pass;
+                URL += '&scope=profile%20openid';
+                window.location.href = URL;
             }
         
         }
